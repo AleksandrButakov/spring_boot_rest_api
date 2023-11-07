96250c1d17593d286c3cf92792bfe7f98ea354e3
AleksandrButakov <anbngm@gmail.com>
2023-11-07 16:11:10 +0500

    init commit

---
+++ src/test/java/ru/anbn/restbackend/RestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.anbn.restbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
